099ac2608d731a63f694e61229af76b305e16247
Anish Lakhwara <anish+git@lakhwara.com>
2024-07-20 18:23:40 -0700

    fix(atomic): add user to systemd unit

--- projects/AtomicData/service.nix
+++ projects/AtomicData/service.nix
@@ -8,47 +8,53 @@ in
 { 
   options = {
     services.atomic-server = {
-      enable = mkEnableOption (lib.mdDoc "Atomic Server");
-      description = lib.mdDoc ''
-          Configuration for Atomic-server, see <https://docs.atomicdata.dev/atomic-data-overview> for documentation.
-        '';
+      enable = mkEnableOption "Enable Atomic Server";
     };
   };
   config = mkIf cfg.enable {
+    users.users.atomic-server = {
+      isSystemUser = true;
+      group = "atomic-server";
+    };
+    users.groups.atomic-server = {};
     systemd.services.atomic-server = {
       description = "Atomic Server";
       after = [ "network.target" ];
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
         ExecStart = "${pkgs.atomic-server}/bin/atomic-server";
-        DynamicUser = true;
+        User = "atomic-server";
+        Environment = [
+          "ATOMIC_CONFIG_DIR=/var/lib/atomic-server"
+          "ATOMIC_DATA_DIR=/var/lib/atomic-server"
+        ];
         StateDirectory = "atomic-server";
         CacheDirectory = "atomic-server";
         WorkingDirectory = "/var/lib/atomic-server";
         RuntimeDirectory = "atomic-server";
         RootDirectory = "/run/atomic-server";
-        ReadWritePaths = "";
+        ReadWritePaths = "/var/lib/atomic-server";
         BindReadOnlyPaths = [
           builtins.storeDir
         ];
         CapabilityBoundingSet = "";
         RestrictAddressFamilies = [ "AF_UNIX" "AF_INET" "AF_INET6" ];
-        RestrictNamespaces = true;
-        PrivateDevices = true;
-        PrivateUsers = true;
-        ProtectClock = true;
-        ProtectControlGroups = true;
-        ProtectHome = true;
-        ProtectKernelLogs = true;
-        ProtectKernelModules = true;
-        ProtectKernelTunables = true;
-        SystemCallArchitectures = "native";
-        SystemCallFilter = [ "@system-service" "~@privileged" ];
-        RestrictRealtime = true;
-        LockPersonality = true;
-        MemoryDenyWriteExecute = true;
-        UMask = "0066";
-        ProtectHostname = true;
+        # RestrictNamespaces = true;
+        # PrivateDevices = true;
+        # PrivateUsers = true;
+        # ProtectClock = true;
+        # ProtectControlGroups = true;
+        # ProtectHome = true;
+        # ProtectKernelLogs = true;
+        # ProtectKernelModules = true;
+        # ProtectKernelTunables = true;
+        # SystemCallArchitectures = "native";
+        # SystemCallFilter = [ "@system-service" "~@privileged" ];
+        # RestrictRealtime = true;
+        # LockPersonality = true;
+        # MemoryDenyWriteExecute = true;
+        # UMask = "0066";
+        # ProtectHostname = true;
       };
     };
   };
